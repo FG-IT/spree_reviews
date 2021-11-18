61c5700a820108430bace67fa59f135979faa5d9
Yong Zhao <oracle.zhao2008@gmail.com>
2021-11-18 09:21:39 -0500

    not insert js to layout, since it's already appened to js

--- app/overrides/add_review_rating_filter.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  virtual_path: 'spree/layouts/spree_application',
-  name: 'add_rating_filter_js',
-  insert_bottom: "[data-hook='inside_head']",
-  text: "<%=  javascript_include_tag('spree/backend/spree_reviews.js', defer: 'defer' )%>"
-)
